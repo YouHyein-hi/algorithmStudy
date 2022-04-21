c1a6feb0cda68fc912e9aa99fa430354aed3874b
Hyein You <laksjd4560@gmail.com>
2022-04-21 23:33:07 +0900

    2022.04.21. 목

---
+++ src/no_2445.java
@@ -0,0 +1,30 @@
+/*
+ * 2022.04.20.수
+ * 2445번 별 찍기-8
+ * */
+import java.util.Scanner;
+
+public class no_2445 {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int num;
+
+        num = s.nextInt();
+
+        for(int i = 1 ; i <= num; i++) {
+            for(int j = 0; j < i; j++) System.out.print("*");
+            for(int k = 2*num-i; k > 0; k--) {
+                if(k>i) System.out.print(" ");
+                else System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for(int i = 2; i <= num; i++) {
+            for(int j = i; j <= num; j++) System.out.print("*");
+            for(int k = 1; k < i*2-1; k++) System.out.print(" ");
+            for(int h = num; h >= i; h--) System.out.print("*");
+            System.out.println();
+        }
+    }
+}
