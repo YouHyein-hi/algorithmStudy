b4dcec3651ac6356113c416483d7caae0a0df7c2
Hyein You <laksjd4560@gmail.com>
2022-06-02 19:06:36 +0900

    수정

--- src/no_11721.java
+++ src/no_11721.java
@@ -1,7 +1,6 @@
 /*
- * 2022.06.02.목
- * 25083번 새싹
- * '단계별로 풀어보기'에서 도전 중이라 되어있는 단계들 나머지 문제 풀기
+ * 2022.06.01.수
+ * 11721번 열 개씩 끊어 출력하기
  * */
 import java.util.Scanner;
 
@@ -9,13 +8,13 @@ public class no_11721 {
     public static void main(String[] args){
         Scanner s = new Scanner(System.in);
 
-        String sprout = "         ,r'\"7\n" +
-                "r`-_   ,'  ,/\n" +
-                " \\. \". L_r'\n" +
-                "   `~\\/\n" +
-                "      |\n" +
-                "      |";
+        String word = s.next();
 
-        System.out.print(sprout);
+        for(int i = 0; i < word.length(); i++){
+            System.out.print(word.charAt(i));
+            if(i % 10 == 9){
+                System.out.println();
+            }
+        }
     }
 }
