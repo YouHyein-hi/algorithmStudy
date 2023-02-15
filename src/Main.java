8d05ae29fe6234ba012da6973870186b019f9ac4
hyein You <laksjd4560@gmail.com>
2023-02-15 20:23:13 +0900

    Main

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+/*
+ * 20**.**.**.*
+ * *번 ~~~~~~~
+ * ~~~~~~~~~~~~~~~~
+ */
+import java.io.IOException;
+
+// 문제 풀 때의 기본 형식
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+    }
+}
